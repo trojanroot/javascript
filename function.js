ef3c194946455559d3a05d298ec7a4aae8bc043e
trojanroot <thapaaryan926@gmail.com>
2026-05-01 21:26:32 +0545

    use of methods

--- function.js
+++ function.js
@@ -19,15 +19,40 @@
 //     console.log(a+b);
 // }
 // arrowsum(5,6);
-// 
 
-    let num=[52,41,90];
+
+// map method
+
+//     let num=[52,41,90];
      
-    let newArr =num.map((val) =>{
-        return val*2;
-    });
-console.log(newArr);
+//     let newArr =num.map((val) =>{
+//         return val*2;
+//     });
+// console.log(newArr);
 
- let calAquare = (num) => {
-    console.log(num*num);
- };
+//  let calAquare = (num) => {
+//     console.log(num*num);
+//  };
+
+// let newArry=num.map((val)=>
+// {
+//     return val;
+// });
+// console.log(newArry)
+
+//filter method
+
+// let nums=[1,2,3,4,5,6,7,8,9];
+// let newArrr=nums.filter((val)=>
+// {
+//     return val%2===0;
+// })
+// console.log(newArrr);
+
+//reduce method
+let arr=[1,2,3,4];
+const o=arr.reduce((result,current)=>
+{
+    return result+current;
+})
+console.log(o);
